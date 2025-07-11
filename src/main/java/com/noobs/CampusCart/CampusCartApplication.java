5a4a0f0fd1cc27d995555bc0e3649581d5571b3e
sayma77 <sayma7504@gmail.com>
2025-07-11 21:48:35 +0600

    initialize project

---
+++ src/main/java/com/noobs/CampusCart/CampusCartApplication.java
@@ -0,0 +1,13 @@
+package com.noobs.CampusCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noobs/CampusCart/CampusCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobs.CampusCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
