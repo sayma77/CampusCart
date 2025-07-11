384520153cc918e4bd16bc2942271dc5e29eaa1b
sayma77 <sayma7504@gmail.com>
2025-07-11 23:10:45 +0600

    initial test run

---
+++ src/main/java/com/noobs/CampusCart/controller/HomePageController.java
@@ -0,0 +1,23 @@
+package com.noobs.campuscart.controller;
+
+import com.noobs.CampusCart.model.Item;
+import com.noobs.CampusCart.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("platformName", "CampusCart");
+        model.addAttribute("user", new User("Sayma"));
+        model.addAttribute("items", List.of(
+            new Item("Textbook", 250),
+            new Item("Lamp", 500)
+        ));
+        return "home"; // refers to home.html in templates/
+    }
+}
---
+++ src/main/java/com/noobs/CampusCart/model/User.java
@@ -0,0 +1,12 @@
+package com.noobs.CampusCart.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String name;
+}
