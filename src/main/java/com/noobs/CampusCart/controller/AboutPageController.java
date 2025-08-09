490e51d6e562ff6fc45c6143c299af4b8a02bba4
Nusrat Jahan <raisanusrat112@gmail.com>
2025-08-09 20:12:20 +0600

    Added About Page Controller and template

--- src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
+++ src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
@@ -1,13 +1,18 @@
 package com.noobs.CampusCart.controller;
-import org.springframework.stereotype.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutPageController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home"; // This will load home.html
+    }
+
     @GetMapping("/about")
     public String aboutPage() {
-        return "about";
+        return "about"; // This will load about.html
     }
 }
-
