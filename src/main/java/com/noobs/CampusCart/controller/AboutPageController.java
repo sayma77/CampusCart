47edb7020cd91caa813f9027251562bd2683451e
sayma77 <sayma7504@gmail.com>
2025-08-11 00:08:58 +0600

    changed color of homepage

--- src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
+++ src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AboutPageController {
 
-    @GetMapping("/")
-    public String homePage() {
-        return "home"; // This will load home.html
-    }
-
     @GetMapping("/about")
     public String aboutPage() {
         return "about"; // This will load about.html
