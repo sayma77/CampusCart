9012c3fd5f0c41317db7c4933f96620c1126b36b
SadiaIshrat <ishratsadia2004@gmail.com>
2025-08-11 00:07:23 +0600

    about page controller fixed

--- src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
+++ src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AboutPageController {
 
-    @GetMapping("/")
-    public String homePage() {
-        return "home"; // This will load home.html
-    }
+    
 
     @GetMapping("/about")
     public String aboutPage() {
