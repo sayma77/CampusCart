32dc8ca565f9a08dd8027655ff9239222b539da5
Nusrat Jahan <raisanusrat112@gmail.com>
2025-08-11 23:37:03 +0600

    Remove unused imports and fix formatting

--- src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
+++ src/main/java/com/noobs/CampusCart/controller/AboutPageController.java
@@ -6,13 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AboutPageController {
 
-    @GetMapping("/")
-    public String homePage() {
-        return "home"; // This will load home.html
-    }
-
     @GetMapping("/about")
     public String aboutPage() {
-        return "about"; // This will load about.html
+        return "about";  // This should map to about.html in templates
     }
 }
+
